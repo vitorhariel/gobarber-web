522d768496ab6ce895a26bb1e9a94d4ecb9eccc5
Vitor Hariel <vhbarauna@gmail.com>
2019-07-21 03:54:39 -0300

    finished gobarber web app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import logo from "~/assets/logo-purple.svg";
@@ -8,6 +9,8 @@ import Notifications from "~/components/Notifications";
 import { Container, Content, Profile } from "./styles";
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,11 +23,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Vitor Hariel</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                "https://api.adorable.io/avatars/50/abott@adorable.png"
+              }
               alt="Vitor Hariel"
             />
           </Profile>
